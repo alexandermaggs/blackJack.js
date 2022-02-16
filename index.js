df3dd56fe8facab9c4376d7a420f97af4cb9c198
alexandermaggs <75097869+alexandermaggs@users.noreply.github.com>
2022-02-16 20:26:26 +0000

    math random added

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-let firstCard = 3;
-let secondCard = 5;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-const messageEl = document.getElementById("message-el");
-const sumEl = document.getElementById("sum-el");
-const cardsEl = document.getElementById("cards-el");
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  return randomNumber;
+}
 
 const startGame = () => {
   renderGame();
@@ -33,9 +37,8 @@ const renderGame = () => {
 };
 
 const newCard = () => {
-  let card = 6;
+  let card = getRandomCard();
   sum += card;
   cards.push(card);
   renderGame();
 };
-// Cash Out
