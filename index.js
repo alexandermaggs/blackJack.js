32d400ecd1a39b6ba5bfccdcc53920bdb56a8826
alexandermaggs <75097869+alexandermaggs@users.noreply.github.com>
2022-02-17 14:59:31 +0000

    main functionality completed

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let player = {
+  name: "Alexander",
+  chips: 0,
+};
+
+let cards = [];
 let hasBlackJack = false;
+let sum;
 let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": £" + player.chips;
+
 function getRandomCard() {
-  let randomNumber = Math.floor(Math.random() * 13) + 1;
-  return randomNumber;
+  let randomNum = Math.floor(Math.random() * 13) + 1;
+  if (randomNum > 10) {
+    return 10;
+  } else if (randomNum === 1) {
+    return 11;
+  } else {
+    return randomNum;
+  }
 }
 
-const startGame = () => {
+function startGame() {
+  // Generate two random numbes
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  // Re-assign the cards and sum variables so that the game can start
+  sum = firstCard + secondCard;
+  cards = [firstCard, secondCard];
   renderGame();
-};
+}
 
 const renderGame = () => {
   cardsEl.textContent = "Cards: ";
@@ -37,8 +56,10 @@ const renderGame = () => {
 };
 
 const newCard = () => {
-  let card = getRandomCard();
-  sum += card;
-  cards.push(card);
-  renderGame();
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
 };
